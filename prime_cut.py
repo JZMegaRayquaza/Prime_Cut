8e290bdb8310b00c9c3bf461f92ed5813ee55e76
JZMegaRayquaza <jzwiicheese@gmail.com>
2025-03-22 15:37:24 -0400

    Initial commit: add steak_crops, prime_cut.py, and steak_half_cuts.json

---
+++ prime_cut.py
@@ -0,0 +1,78 @@
+import cv2
+import os
+import json
+
+def crop(frame, x=75, y=225, w=500, h=225):
+    '''
+    Crops frame to show steak by default.
+    '''
+    return frame[y:y+h, x:x+w]
+
+def find_steak_shape(cropped_frame):
+    '''
+    Find closest match in steak_crops.
+    '''
+    input_dir = 'steak_crops/'
+
+    for steak_crop in os.listdir(input_dir):
+        input_path = os.path.join(input_dir, steak_crop)
+        template = cv2.imread(input_path)
+
+        # Match current steak shape with all possible steak shapes
+        result = cv2.matchTemplate(cropped_frame, template, cv2.TM_CCOEFF_NORMED)
+        _, max_val, _, _ = cv2.minMaxLoc(result)
+
+        # If steak is similar enough (return steak shape)
+        if max_val > 0.9:
+            steak = os.path.splitext(steak_crop)[0].split('_')[0]
+            return steak[-1]
+
+# Load steak cut dictionary
+with open('steak_half_cuts.json', 'r') as f:
+    cut_dict = json.load(f)
+
+video_source = 0  # Elgato
+
+# Open the video capture
+cap = cv2.VideoCapture(video_source)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1440)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+cap.set(cv2.CAP_PROP_FPS, 60)
+
+# Read one frame to get frame size
+ret, frame = cap.read()
+
+frame_height, frame_width = frame.shape[:2]
+fps = 30
+output_filename = 'prime_cut_opencv.mp4'
+
+# Create VideoWriter
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter(output_filename, fourcc, fps, (frame_width, frame_height))
+
+print(f'Recording started: {output_filename}')
+
+# Main loop
+while True:
+    ret, frame = cap.read()
+
+    cropped = crop(frame)
+    steak = find_steak_shape(cropped)
+
+    if steak in cut_dict:
+        height, width, _ = frame.shape
+        x1 = 75 + cut_dict[steak]
+        x2 = int(x1 + width / 2)
+
+        cv2.line(frame, (x1, 0), (x1, height), (0, 0, 255), 2)
+        cv2.line(frame, (x2, 0), (x2, height), (0, 0, 255), 2)
+
+    out.write(frame)  # Write frame to video
+    cv2.imshow('Prime Cut', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
