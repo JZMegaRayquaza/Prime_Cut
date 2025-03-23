04cf08cd58ca3b685071e522e2f1e6cd25325642
JZMegaRayquaza <jzwiicheese@gmail.com>
2025-03-23 14:39:02 -0400

    removed app.py

--- prime_cut.py
+++ prime_cut.py
@@ -27,37 +27,41 @@ def find_steak_shape(cropped_frame):
             steak = os.path.splitext(steak_crop)[0].split('_')[0]
             return steak[-1]
 
-# Load steak cut dictionary
-with open('steak_half_cuts.json', 'r') as f:
-    cut_dict = json.load(f)
+def run_prime_cut():
+    # Load steak cut dictionary
+    with open('steak_half_cuts.json', 'r') as f:
+        cut_dict = json.load(f)
 
-video_source = 0  # Elgato
+    video_source = 0  # Elgato
 
-# Open the video capture
-cap = cv2.VideoCapture(video_source)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1440)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-cap.set(cv2.CAP_PROP_FPS, 60)
+    # Open the video capture
+    cap = cv2.VideoCapture(video_source)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1440)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+    cap.set(cv2.CAP_PROP_FPS, 60)
 
-# Main loop
-while True:
-    ret, frame = cap.read()
+    # Main loop
+    while True:
+        ret, frame = cap.read()
 
-    cropped = crop(frame)
-    steak = find_steak_shape(cropped)
+        cropped = crop(frame)
+        steak = find_steak_shape(cropped)
 
-    if steak in cut_dict:
-        height, width, _ = frame.shape
-        x1 = 75 + cut_dict[steak]
-        x2 = int(x1 + width / 2)
+        if steak in cut_dict:
+            height, width, _ = frame.shape
+            x1 = 75 + cut_dict[steak]
+            x2 = int(x1 + width / 2)
 
-        cv2.line(frame, (x1, 0), (x1, height), (0, 0, 255), 2)
-        cv2.line(frame, (x2, 0), (x2, height), (0, 0, 255), 2)
+            cv2.line(frame, (x1, 0), (x1, height), (0, 0, 255), 2)
+            cv2.line(frame, (x2, 0), (x2, height), (0, 0, 255), 2)
 
-    cv2.imshow('Prime Cut (press q to quit)', frame)
+        cv2.imshow('Prime Cut (press q to quit)', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    run_prime_cut()
